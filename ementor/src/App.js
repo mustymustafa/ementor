0790938d1e70ecfd7fd792b8f20e0a2ba6f89d04
mustapha <mustafaumar70@live.com>
2018-11-26 16:15:58 +0100

    chidi

--- ementor/src/App.js
+++ ementor/src/App.js
@@ -69,7 +69,6 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:username" component={Username} />
               <Route exact path="/sasprofiles" component={SasProfile} />
               <Route exact path="/sbeprofiles" component={SbeProfile} />
               <Route exact path="/solprofiles" component={SolProfile} />
@@ -77,6 +76,13 @@ class App extends Component {
               <Switch>
                 {" "}
                 <PrivateRoute exact path="/profile" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/profiles/:username"
+                  component={Username}
+                />
               </Switch>
               <Switch>
                 {" "}
